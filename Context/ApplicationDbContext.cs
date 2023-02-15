0f8c1d5fb9244658e3000c70aa7f28b8d95abbcb
Emilio Lubo <emilubo@msn.com>
2023-02-15 17:29:41 -0300

    Crud basico terminado

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudNet7MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet7MVC.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+        
+        public DbSet<Contacto> Contactos { get; set; }
+    }
+}
---
+++ Controllers/InicioController.cs
@@ -0,0 +1,107 @@
+﻿using CrudNet7MVC.Context;
+using CrudNet7MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace CrudNet7MVC.Controllers
+{
+    public class InicioController : Controller
+    {
+        private readonly ApplicationDbContext _contexto;
+
+        public InicioController(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _contexto.Contactos.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Contacto contacto)
+        {
+            if(ModelState.IsValid)
+            {
+                _contexto.Contactos.Add(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var contacto = _contexto.Contactos.Find(id);
+            if (contacto == null) return NotFound();
+
+            return View(contacto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Update(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Detalle(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var contacto = _contexto.Contactos.Find(id);
+            if (contacto == null) return NotFound();
+
+            return View(contacto);
+        }
+
+        [HttpGet]
+        public IActionResult Eliminar(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var contacto = _contexto.Contactos.Find(id);
+            if (contacto == null) return NotFound();
+
+            return View(contacto);
+        }
+
+        [HttpPost, ActionName("Eliminar")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EliminarContacto(int? id)
+        {
+            var contacto = await _contexto.Contactos.FindAsync(id);
+            if(contacto == null) return View();
+
+            _contexto.Contactos.Remove(contacto);
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
